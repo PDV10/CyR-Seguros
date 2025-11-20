ac06ea07b23466ba60a18e1d539d75425d6d0826
PDV10 <patooodv@gmail.com>
2025-11-20 16:44:02 -0300

    se elimina console log de prueba

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -81,9 +81,6 @@ export default function ContactForm({
 
       onSubmit?.(values);
       setIsSubmitting(true);
-      console.log("EMAILJS_SERVICE_ID", EMAILJS_SERVICE_ID);
-      console.log("EMAILJS_TEMPLATE_ID", EMAILJS_TEMPLATE_ID);
-      console.log("EMAILJS_PUBLIC_KEY", EMAILJS_PUBLIC_KEY);
 
       try {
         const templateParams = {
